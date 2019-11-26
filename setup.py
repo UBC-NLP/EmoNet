64c9efcc89be943129757bb848f890445582047e
hazadeh <xlbrlx@yahoo.com>
2019-11-25 18:27:31 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='emonet',
       packages=['emonet'],
       install_requires=[
           'happiestfuntokenizing',
-          'numpy', 'pandas', 'gensim'
+          'numpy', 'pandas', 'gensim'و
           'torch',
           'transformers'
       ],
